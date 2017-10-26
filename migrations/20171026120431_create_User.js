6fe8bdf15ec10edf880d1502fc0aea6b6466eaba
ChuckHagy <chuckhagy@gmail.com>
2017-10-26 13:44:40 -0700

    user migration and seed

--- migrations/20171026120431_create_User.js
+++ migrations/20171026120431_create_User.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('User', table => {
+        table.increments();
+        table.specificType('username', 'char(32)').unique().notNullable();
+        table.specificType('hashedPassword', 'char(60)').notNullable();
+        table.specificType('displayName', 'char(32)').notNullable();
+        table.text('statusMessage');
+        table.specificType('email', 'char(60)').unique().notNullable();
+        table.timestamps(true, true);
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('User')
 };
