5964218dd7bb447ed0c2f49cfa6aaf35aaabd55f
ChuckHagy <chuckhagy@gmail.com>
2017-10-28 09:16:46 -0700

    first endpoint working with pattern CREATE USER

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../lib/instances/usersController');
+const usersController = require('../lib/instances/userController');
 
-router.post('/user', userController.createUser);
+router.get('/', (req, res, next) => {
+    res.send('Hello There, we are running')
+});
+
+
+// router.get('/user/:userid/goal-assignments');
+
+
+router.post('/users', userController.create);
+
+
 
 module.exports = router;
