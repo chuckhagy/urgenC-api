076aad22ae32f84034da67ec2c3edb8c11259730
ChuckHagy <chuckhagy@gmail.com>
2017-10-28 12:41:27 -0700

    delete working

--- routes/user.js
+++ routes/user.js
@@ -7,15 +7,11 @@ router.get('/users', userController.getAll);
 router.post('/users', userController.create);
 router.get('/users/:userId', userController.getById);
 router.patch('/users/:userId', userController.updateById);
+router.delete('/users/:userId', userController.deleteById);
 
 
 
 
-
-
-router.delete('/users/:userId', (req, res, next) => {
-    res.status(200).send('Hello, I am running')
-});
 
 router.get('/users/:userId/goal-assignments', (req, res, next) => {
     res.status(200).send('Hello, I am running')
