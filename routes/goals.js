644bd81863ea53936f483e4420a05a9c900b7b80
ChuckHagy <chuckhagy@gmail.com>
2017-10-28 13:24:58 -0700

    can now get goals by owner

--- routes/goals.js
+++ routes/goals.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const goalsController = require('../lib/instances/goalController');
 
 router.get('/goals', goalsController.getAll);
+router.get('/goals/:ownerUserId', goalsController.getByOwnerUserId);
 router.post('/goals', goalsController.create);
 router.patch('/goals/:goalId', goalsController.updateById);
-
-
-
 router.delete('/goals/:goalId', goalsController.deleteById);
 
 module.exports = router;
