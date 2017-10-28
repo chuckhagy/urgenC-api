15423568eba0d2b08f311184d249371d94f6948b
ChuckHagy <chuckhagy@gmail.com>
2017-10-28 13:07:07 -0700

    can add goals

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { JWT_KEY } = require('./env');
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const app  = express();
 const user = require('./routes/user');
--- routes/goals.js
+++ routes/goals.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const goalsController = require('../lib/instances/goalController');
 
 router.get('/goals', goalsController.getAll);
+
+
+
 router.post('/goals', goalsController.create);
 router.patch('/goals/:goalId', goalsController.updateById);
 router.delete('/goals/:goalId', goalsController.deleteById);
