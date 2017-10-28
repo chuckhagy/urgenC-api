67636e8fdf4eff978fe6309ed2d00d6cc8cdd31c
ChuckHagy <chuckhagy@gmail.com>
2017-10-28 09:40:58 -0700

    cleared idea dir

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const { JWT_KEY } = require('./env');
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 const app  = express();
 const user = require('./routes/user');
+
+//for dev purposes below
+app.get('/', (req, res) => {
+    res.send('We are up and running')
+})
 
 app.use(bodyParser.json());
 
