3206d8a5849182be64386927cc92581cdf3944a7
ChuckHagy <chuckhagy@gmail.com>
2017-10-28 13:10:41 -0700

    update goals working

--- routes/goals.js
+++ routes/goals.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const goalsController = require('../lib/instances/goalController');
 
 router.get('/goals', goalsController.getAll);
-
-
-
 router.post('/goals', goalsController.create);
 router.patch('/goals/:goalId', goalsController.updateById);
+
+
+
 router.delete('/goals/:goalId', goalsController.deleteById);
 
 module.exports = router;
