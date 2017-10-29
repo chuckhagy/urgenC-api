e7c3c14c6b9da098f78dbd2cdf81016470008f7e
ChuckHagy <chuckhagy@gmail.com>
2017-10-29 09:35:54 -0700

    one test working and now attaching userId to request via JWT

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 const { JWT_KEY } = require('./env');
+const jwt = require('express-jwt');
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8000;
-
 const app  = express();
+
 const user = require('./routes/user');
 const goals = require('./routes/goals');
 const authentication = require('./routes/authentication');
 
-//for dev purposes below
-app.get('/', (req, res) => {
-    res.send('We are up and running')
-})
-
 app.use(bodyParser.json());
+
+app.use(
+    jwt({
+        secret: JWT_KEY,
+        requestProperty: 'jwt.payload',
+        credentialsRequired: false,
+        audience: 'urgenC',
+        issuer: 'urgenC'
+    })
+);
+
+app.use((req, res, next) => {
+    let authUserId = req.jwt ? req.jwt.payload.sub : undefined;
+    if(typeof authUserId === 'number' && authUserId > 0) {
+        req.authenticatedUserId = authUserId;
+    } else req.authenticatedUserId = null;
+    // console.log(req.authenticatedUserId);
+    // console.log(req.jwt);
+    console.log(req.jwt);
+    next();
+});
 
 app.use(user);
 app.use(goals);
 app.use(authentication);
+
+app.all('*', (request, response, next) => {
+    response.sendStatus(404)
+});
 
 app.listen(PORT, () => {
     console.log('Listening on ', PORT)
