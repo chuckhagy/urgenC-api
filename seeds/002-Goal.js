616388a8872858d07c1ab0fc89e796f7f83885dd
ChuckHagy <chuckhagy@gmail.com>
2017-11-02 12:58:41 -0700

    more useful seed

--- seeds/002-Goal.js
+++ seeds/002-Goal.js
@@ -4,13 +4,13 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return knex('Goal').insert([
           {title: 'Buy a new car', body: 'N/A', dueDate: '2017-12-31T16:30:00.000Z', priority: 4, ownerUserId: 1},
-          {title: 'Buy a new car', body: 'N/A', dueDate: '2017-11-30T16:30:00.000Z', priority: 3, ownerUserId: 1},
-          {title: 'Buy a new car', body: 'N/A', dueDate: '2017-12-30T16:30:00.000Z', priority: 4, ownerUserId: 2},
-          {title: 'Buy a new car', body: 'N/A', dueDate: '2017-11-25T16:30:00.000Z', priority: 2, ownerUserId: 2},
-          {title: 'Buy a new car', body: 'N/A', dueDate: '2018-01-01T16:30:00.000Z', priority: 5, ownerUserId: 3},
-          {title: 'Buy a new car', body: 'N/A', dueDate: '2017-12-20T16:30:00.000Z', priority: 2, ownerUserId: 3},
-          {title: 'Buy a new car', body: 'N/A', dueDate: '2017-12-11T16:30:00.000Z', priority: 4, ownerUserId: 4},
-          {title: 'Buy a new car', body: 'N/A', dueDate: '2017-12-15T16:30:00.000Z', priority: 1, ownerUserId: 4},
+          {title: 'Buy a new house', body: 'N/A', dueDate: '2017-11-30T16:30:00.000Z', priority: 3, ownerUserId: 1},
+          {title: 'Buy a new boat', body: 'N/A', dueDate: '2017-12-30T16:30:00.000Z', priority: 4, ownerUserId: 2},
+          {title: 'Buy a new shoe', body: 'N/A', dueDate: '2017-11-25T16:30:00.000Z', priority: 2, ownerUserId: 2},
+          {title: 'Buy a new candy', body: 'N/A', dueDate: '2018-01-01T16:30:00.000Z', priority: 5, ownerUserId: 3},
+          {title: 'Buy a new friend', body: 'N/A', dueDate: '2017-12-20T16:30:00.000Z', priority: 2, ownerUserId: 3},
+          {title: 'Buy a new phone', body: 'N/A', dueDate: '2017-12-11T16:30:00.000Z', priority: 4, ownerUserId: 4},
+          {title: 'Buy a new drink', body: 'N/A', dueDate: '2017-12-15T16:30:00.000Z', priority: 1, ownerUserId: 4},
       ]);
     });
 };
--- seeds/003-GoalAssignment.js
+++ seeds/003-GoalAssignment.js
@@ -4,12 +4,18 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return knex('GoalAssignment').insert([
           {userId: 2, goalId: 2, status: 'current'},
-          {userId: 1, goalId: 1, status: 'current'},
           {userId: 3, goalId: 3, status: 'current'},
           {userId: 4, goalId: 4, status: 'current'},
+
           {userId: 2, goalId: 1, status: 'current'},
           {userId: 3, goalId: 1, status: 'current'},
           {userId: 4, goalId: 1, status: 'current'},
+          {userId: 2, goalId: 1, status: 'current'},
+
+          {userId: 1, goalId: 2, status: 'current'},
+          {userId: 3, goalId: 2, status: 'current'},
+          {userId: 4, goalId: 2, status: 'current'},
+
       ]);
     });
 };
